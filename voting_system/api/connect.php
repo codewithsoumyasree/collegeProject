154a3aec4fd0339797f79b7949ee566f6a576993
codewithsoumyasree <soumyasreeacharjee@gmail.com>
2024-12-24 21:37:36 +0530

    Final touch: html and css modification

--- voting_system/api/connect.php
+++ voting_system/api/connect.php
@@ -2,15 +2,5 @@
 
 $connect = mysqli_connect("localhost", "root", "", "voting_system") or die("connection failed!");
 
-// if($connect){
-
-// echo "Connected!";
-
-// }
-
-// else{
-
-// echo "Not Connected!";
-// }
 
 ?>
